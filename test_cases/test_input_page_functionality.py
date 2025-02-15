58773f40cdd7b4d30b1cdc93310f11395679c37b
gokuldev <gokuldev.p123@gmail.com>
2025-02-15 19:04:28 +0530

    Update menu card

--- test_cases/test_input_page_functionality.py
+++ test_cases/test_input_page_functionality.py
@@ -20,23 +20,23 @@ class TestInputPageFunctionality:
         """
         Test to open the Letcode test page and interact with the specified menu card.
 
-        This test opens the Letcode test page and navigates to the section specified 
-        by the 'menu_card' parameter by clicking on the corresponding menu card.
+        This test opens the Letcode test page and performs various interactions
+        based on the specified 'device' type.
 
         :param device: The device type for viewport size (e.g., desktop, tablet, mobile).
-        :param menu_card: The menu card to interact with (e.g., 'Input', 'Buttons').
         """
         # Open Letcode test page
         self.card_page.open_letcode_website(device)
         self.card_page.click_navbar_burger_icon(device)
         self.card_page.navigate_to_testing_workspace()
-        self.card_page.click_menu_card(MENU_CARDS['Input'])
+        self.card_page.click_menu_card('Input')
         self.input_page.enter_your_full_name("Gokul Dev")
-        self.input_page.append_a_text_and_press_keyboard_tab("How are you doign")
+        self.input_page.append_a_text_and_press_keyboard_tab("How are you doing")
         self.input_page.what_is_inside_the_get_me_text_box()
         self.input_page.clear_the_text_inside_clear_text_box()
         self.input_page.confirm_edit_field_is_disabled_noedit_text_box()
         self.input_page.confirm_text_is_readonly_dontwrite_text_box()
+
 
 
 
